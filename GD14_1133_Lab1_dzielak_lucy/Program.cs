b42e2c939f6e145c1ade6135771f25795976a7b6
LucyDz <dzielaklucy@gmail.com>
2025-09-05 20:52:55 -0700

    Exploring Visual Studio
    Basic Project with an extra line from Lab1

---
+++ GD14_1133_Lab1_dzielak_lucy/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GD14_1133_Lab1_dzielak_lucy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Lucy Dzielak");
+        }
+    }
+}
