5795f4e384e67281a438954e68e2d0843b3d7133
LucyDz <dzielaklucy@gmail.com>
2025-09-18 15:54:53 -0700

    Created Files
    Created Dice Game .cs files

---
+++ DiceGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using GD14_1133_DiceGame_Lucy.Scripts;
+
+namespace GD14_1133_DiceGame_Lucy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameManager.ProgramStart();
+        }
+    }
+}
