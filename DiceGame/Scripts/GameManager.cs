50cb2385b1a3ccf64c1d66f0d8a758416ff23893
Lucy <dzielaklucy@gmail.com>
2025-09-26 18:45:44 -0700

    Full Round
    Finished building 1 full round

--- DiceGame/Scripts/GameManager.cs
+++ DiceGame/Scripts/GameManager.cs
@@ -6,117 +6,259 @@ using System.Threading.Tasks;
 
 namespace GD14_1133_DiceGame_Lucy.Scripts
 {
-    internal class DieRoller
-    { 
-        // created random class instance.
-        Random Random = new Random();
-
-        internal void Roll()
-        {
-            int d6 = Random.Next(1, 7); //created random number roller for d6 die.
-            int d8 = Random.Next(1, 9); //created random number roller for d8 die.
-            int d12 = Random.Next(1, 13); //created random number roller for d12 die.
-            int d20 = Random.Next(1, 21); //created random number roller for d20 die.
-            int total = d6 + d8 + d12 + d20; // created total sum counter.
-
-            //printed roll outcomes.
-            Console.WriteLine("Your d6 result is " + d6 + "!");
-            Console.WriteLine("Your d8 result is " + d8 + "!");
-            Console.WriteLine("Your d12 result is " + d12 + "!");
-            Console.WriteLine("Your d20 result is " + d20 + "!");
-
-            //printed total outcome.
-            Console.WriteLine("Your total sum is " + total + "!");
-        }
-    
-    }
-    internal class Operators
+    internal class Die
     {
-        //created function to show examples of Arithmetic Operators.
-        internal void Arithmetics()
+        //creates a random instance so each roll is different
+        private Random random = new Random();
+        internal int d6() // rolls a d6 
         {
-            int a = 1;
-            int b = 2;
-            int c = 4;
-            int d = 5;
-
-            //explained what each operator does. 
-            Console.WriteLine("");
-            Console.WriteLine("I will now explain what operators do and give examples");
-            Console.WriteLine("The operators (+,-,++,--,*,/, and %) function as follows");
-            Console.WriteLine(" + adds two integers together");
-            Console.WriteLine(" - subtracts an integer from the other");
-            Console.WriteLine(" ++ increases an integer's value by 1");
-            Console.WriteLine(" -- decreases an integer's value by 1");
-            Console.WriteLine(" * multiplies two integers together");
-            Console.WriteLine(" / divides one integer by the other");
-            Console.WriteLine("");
-            
-            //performed examples of each operator.
-            Console.WriteLine("I will now show some examples");
-           
-            Console.WriteLine("");
-            Console.WriteLine("lets use + to create the sum of 1 + 2");
-            Console.WriteLine(a + b);
-            Console.WriteLine("our result was 3");
-           
-            Console.WriteLine("");
-            Console.WriteLine("lets use - to subtract 1 from 2");
-            Console.WriteLine(b - a);
-            Console.WriteLine("our result was 1");
-            
-            Console.WriteLine("");
-            Console.WriteLine("lets use ++ to increase 2 by one increment");
-            Console.WriteLine(++b);
-            Console.WriteLine("our result was 3");
-           
-            Console.WriteLine("");
-            Console.WriteLine("lets use -- to decrease 3 by one increment");
-            Console.WriteLine(--b);
-            Console.WriteLine("our result was 2");
-           
-            Console.WriteLine("");
-            Console.WriteLine("lets use * to multiply 2 by 2");
-            Console.WriteLine(b * b);
-            Console.WriteLine("our result was 4");
-
-            Console.WriteLine("");
-            Console.WriteLine("lets use / to divide 2 by 1");
-            Console.WriteLine(b / a);
-            Console.WriteLine("our result was 2");
-
-            Console.WriteLine("");
-            Console.WriteLine("lets use % to find the remainder of 5 - (5 / 4) x 4");
-            Console.WriteLine(d % c);
-            Console.WriteLine("our result is 1 because 'int' variables ignore decimal values");
-
-        }   
+            return random.Next(1, 7);
+     
+        }
+        internal int d8() // rolls a d8 
+        {
+            return random.Next(1, 9);
+        }
+        internal int d12() // rolls a d12 
+        {
+            return random.Next(1, 13);
+        }
+        internal int d20() // rolls a d20
+        {
+            return random.Next(1, 21);
+        }
 
     }
 
 
+    internal class Player
+    {
+        int playerScore; 
+        string playerName = "Player"; // sets player name to "Player" by default just to instantiate it out here
+        // wanted to bring this name so it could be put on the scoreboard at the end of a round but couldn't figure it out
+        internal string GetPlayerName()
+        {
+            
+
+            //ask player to enter name
+            Console.WriteLine("Please enter your name");
+
+            //player gives their name
+            playerName = Console.ReadLine();
+
+            Console.WriteLine("Alright " + playerName + ", Let's play!");
+
+            return playerName;
+        }
+
+        internal void InitializePlayerScore()
+        {
+            // set playerScore to 0 because they start with nothing
+            playerScore = 0;
+        }
+         
+        internal int FetchPlayerScore() // trying to make a way to bring the score to another Class
+        {
+            return playerScore;
+        }
+        // couldn't figure out how to make another GameManager Class call these scores and be able to add to them. So I just had different variables store the scores in the GameManager...
+    }
 
     internal class GameManager
     {
-        internal void Play()
+        //setting new player and cpu
+        Player user = new Player();
+
+        Player cpu = new Player();
+
+        //making scores 0 here because I can't figure out how to summon the scores from Player Class
+        internal int cpuScore = 0;
+
+        internal int userScore = 0;
+       
+        //created variable to store players roll
+        int playerRoll = 0;
+
+        //created variable to store cpu roll
+        int cpuRoll = 0;
+        internal void Play() 
         {
             //welcome message.
-            Console.WriteLine("Welcome to Dice Game! My name is Lucy and today is 2025-09-18");
+            Console.WriteLine("Welcome to Dice Game! My name is Lucy and today is 2025-09-26");
             Console.WriteLine("");
 
-            //created an instance to call DieRoller to GameManager.
-            DieRoller DieRollerInstance = new DieRoller();
+            //calling function to get players info
+            user.GetPlayerName();
+            user.InitializePlayerScore(); // these ended up not being used because I can't figure out how to call the score from Player to GameManager
+            cpu.InitializePlayerScore();  // these ended up not being used because I can't figure out how to call the score from Player to GameManager
+            Coinflip();
+        }
+        internal int Coinflip() // decides who goes first
+        {
+            //explaining the coinflip to decide first
+            Console.WriteLine(""); 
+            Console.WriteLine("Lets flip a coin to see who goes first");
+            Console.WriteLine("If it's heads, I'll go first, if it's tails, you will go first.");
 
-            //called Roll function from within DieRollerInstance Class.
-            DieRollerInstance.Roll();
+            // making the computer pick 0 or 1
+            Random flip = new Random();
+            int flipResult = flip.Next(0,2);
 
-            //instantiated and called Operatorinstance to run Arithmetics.
-            Operators operatorinstance = new Operators();
-            operatorinstance.Arithmetics();
-
-            //goodbye message.
+            
+            //0 is heads so the GameManager calls the cpu turn first
+            if (flipResult == 0)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Heads!");
+                Console.WriteLine("");
+                Console.WriteLine("It's my turn first...");
+                CpuTurn();
+                Console.WriteLine("");
+                Console.WriteLine("It's now your turn...");
+                PlayerTurn();
+                ScoreCheck();
+            }
+            //1 is tails so the GameManager calls the players turn first
+            else if (flipResult == 1)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Tails!");
+                Console.WriteLine("");
+                Console.WriteLine("You go first...");
+                PlayerTurn();
+                Console.WriteLine("");
+                Console.WriteLine("It's now my turn...");
+                CpuTurn();
+                ScoreCheck();
+            }
+            return flipResult;
+        }
+        internal void PlayerTurn()
+        {
+            //player gets dice options
             Console.WriteLine("");
-            Console.WriteLine("Thank you for rolling and viewing! Goodbye.");
+            Console.WriteLine("Please choose a dice to roll");
+            Console.WriteLine("Your choices are; d6, d8, d12, or d20");
+            
+            //player enters which die they would like to roll
+            string dieInput = Console.ReadLine();
+            Die die = new Die();
+
+            
+            // if statements to call each possible die choice
+            if (dieInput == "d6")
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Rolling your d6...");
+                playerRoll = die.d6(); // calls d6 from Die Class
+            }
+
+            else if (dieInput == "d8")
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Rolling your d8...");
+                playerRoll = die.d8(); // calls d8 from Die Class
+            }
+
+            else if (dieInput == "d12")
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Rolling your d12...");
+                playerRoll = die.d12(); // calls d12 from Die Class
+            }
+
+            else if (dieInput == "d20")
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Rolling your d20...");
+                playerRoll = die.d20(); // calls d20 from Die Class
+            }
+            else 
+            { 
+                Console.WriteLine("please input a valid dice"); // asks them to pick again if the typed something else
+                PlayerTurn();
+            }
+            // roll result printed
+                Console.WriteLine("Your " + dieInput + " rolled " + playerRoll + " !");     
+        }
+        internal void CpuTurn()
+        {
+            //created a new die instance
+            Die die = new Die();
+
+            //making the computer pick a random number from 1 - 4
+            Random diceRandom = new Random();
+            int diceChoice = diceRandom.Next(1, 5);
+
+            //making the chosen number align with and roll one of the dice
+            if (diceChoice == 1) 
+            {
+                Console.WriteLine("");
+                Console.WriteLine("I'll roll my d6...");
+                cpuRoll = die.d6(); // calls d6 from Die Class
+                Console.WriteLine("My d6 rolled " + cpuRoll + " !");
+            }
+            else if (diceChoice == 2)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("I'll roll my d8...");
+                cpuRoll = die.d8(); // calls d8 from Die Class
+                Console.WriteLine("My d8 rolled " + cpuRoll + " !");
+            }
+            else if (diceChoice == 3)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("I'll roll my d12...");
+                cpuRoll = die.d12(); // calls d12 from Die Class
+                Console.WriteLine("My d12 rolled " + cpuRoll + " !");
+            }
+            else if (diceChoice == 4)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("I'll roll my d20...");
+                cpuRoll = die.d20(); // calls d20 from Die Class
+                Console.WriteLine("My d20 rolled " + cpuRoll + " !");
+            }
+
+        }
+        internal void ScoreCheck() // creating a function to check the score and show a scoreboard
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Lets see who won that round!");
+            if (cpuRoll > playerRoll)
+            {
+                cpu.FetchPlayerScore();// trying to fetch the scores from the Player Classes here but not sure what to do with them afterwards
+                cpuScore++; //just added to GameManager stored score instead
+            }
+            else if (cpuRoll < playerRoll)
+            {
+                user.FetchPlayerScore();// trying to fetch the scores from the Player Classes here but not sure what to do with them afterwards
+                userScore++; //just added to GameManager stored score instead
+            }
+
+            //scoreboard display
+            Console.WriteLine("");
+            Console.WriteLine("=============================");
+            Console.WriteLine("         SCOREBOARD          ");
+            Console.WriteLine("         Lucy: " + cpuScore);
+            Console.WriteLine("         You: " + userScore);
+            Console.WriteLine("=============================");
+
+            //if statement to write a line depending on who won the round
+            if (cpuScore > userScore)
+            {
+                Console.WriteLine("");   
+                Console.WriteLine("Looks like I win this round!");
+            }
+            else if (cpuScore < userScore)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Looks like you win this round!");
+            }
+
+            //outro message
+            Console.WriteLine("");
+            Console.WriteLine("Thanks for playing my game!");
         }
     }
-}
+} 
