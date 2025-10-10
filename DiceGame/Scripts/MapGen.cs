19828bb72119f2e7076872866964b8aaa7e0072b
Lucy <dzielaklucy@gmail.com>
2025-10-09 18:24:59 -0700

    Created HauntedHouse Layout
    - House layout works
    - spawns player in random room
    - rooms are linked
    - visual representation of rooms

--- DiceGame/Scripts/MapGen.cs
+++ DiceGame/Scripts/MapGen.cs
@@ -8,19 +8,93 @@ namespace GD14_1133_DiceGame_Lucy.Scripts
 {
     internal class MapGen
     {
-        public void GenerateHouse()
+        public static void HouseMap(Room[,] house, Room playerRoom) // visualize player location
         {
-            int[,] house =
-        {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-        };
-
-            Console.WriteLine(house + " Generated");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (house[i, j] == playerRoom)
+                        Console.Write("[X]");
+                    else
+                    {
+                        Console.Write("[ ]");
+                    }
+                }
+                Console.WriteLine();
+            }
         }
-        
-    
+        public void Start()
+        {
+            Console.WriteLine("Welcome to the Haunted House");
+            Room[,] house = new Room[3, 3];
+            int roomNumber = 0;
+            Random random = new Random();
+
+            //generate house layout
+            for (int y = 0; y < house.GetLength(0); y++)
+            {
+                for (int x = 0; x < house.GetLength(1); x++)
+                {
+                    int roomType = random.Next (0, 3);
+                    switch(roomType)
+                    {
+                        case 0:
+                            house[y, x] = new Room.EmptyRoom(roomNumber, y, x); 
+                            break;
+                       
+                        case 1:
+                            house[y, x] = new Room.TreasureRoom(roomNumber, y, x);
+                            break;
+                        
+                        case 2:
+                            house[y, x] = new Room.CombatRoom(roomNumber, y, x);
+                            break;
+                        
+                        default:
+                            house[y, x] = new Room.EmptyRoom(roomNumber, y, x);
+                            break;
+                    }
+                    roomNumber++;
+                }
+            }
+            for (int y = 0; y < house.GetLength(0); y++) // linking rooms
+            {
+                for (int x = 0; x < house.GetLength(1); x++)
+                {
+                   Room currentRoom = house[y, x];
+                   if (y > 0)
+                        currentRoom.North = house[y - 1, x];
+                   if (y < house.GetLength(0) - 1)
+                        currentRoom.South = house[y + 1, x];
+                   if (x > 0)
+                        currentRoom.West = house[y, x - 1];
+                   if (x < house.GetLength(1) - 1)
+                        currentRoom.East = house[y, x + 1];
+
+
+
+
+
+
+
+                }
+            }
+            //spawns player in random room
+            random = new Random();
+
+            int randx = random.Next(0, 3);
+            int randy = random.Next(0, 3);
+            Room current = house[randy, randx];
+            current.OnRoomEntered();
+            current.RoomDescription();
+            HouseMap(house, current);
+        }
+
+
+
+
+
 
     }
 }
--- DiceGame/Scripts/Room.cs
+++ DiceGame/Scripts/Room.cs
@@ -8,29 +8,106 @@ namespace GD14_1133_DiceGame_Lucy.Scripts
 {
     internal abstract class Room
     {
+        public bool WasVisited { get; set; }
+        public int RoomNumber { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public Room North { get; set; }
+        public Room South { get; set; }
+        public Room West { get; set; }
+        public Room East { get; set; }
+
         public void RoomDescription()
         {
+            if (!WasVisited)
+            {
+                Console.WriteLine("You've already been in this room");
+            }
+            else
+            {
 
+            }
         }
         public void OnRoomEntered()
         {
-
+            Console.WriteLine("You enter the next room...");
         }
         public void OnRoomSearched()
         {
-
+            Console.WriteLine("You search the room");
         }
         public void OnRoomExit()
         {
-
+            Console.WriteLine("You leave this room");
         }
-    }
-    internal class TreasureRoom : Room
-    {
+        public Room(int roomNumber, int row, int column)
+        {
+            RoomNumber = roomNumber;
+            Row = row;
+            Column = column;
+            WasVisited = false;
+        }
+        
+        internal class TreasureRoom : Room
+        {
+            public TreasureRoom(int roomNumber, int row, int column) : base(roomNumber, row, column)
+            {
+
+            }
+        }
+        internal class EmptyRoom : Room
+        {
+            public EmptyRoom(int roomNumber, int row, int column) : base(roomNumber, row, column)
+            {
+
+            }
+        }
+
+        internal class CombatRoom : Room
+        {
+            public CombatRoom(int roomNumber, int row, int column) : base(roomNumber, row, column)
+            {
+
+            }
+        }
+
+         
+
+
+
+
+
+
+
+
+        public void MoveRooms(int x, int y, string direction)
+        {
+            switch (direction)
+            {
+                case "north":
+                    break;
+
+                case "south":
+                    break;
+
+                case "west":
+                    break;
+
+                case "east":
+                    break;
+
+            }
+        }
+
+
+
+
+
 
     }
-    internal class CombatRoom : Room
-    {
+}    
 
-    }
-}
+
+
+
+    
